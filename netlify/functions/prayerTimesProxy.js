a87c5e3c1e26af902b68acdccc46d44da3d215c2
midito <miditochaoui@gmail.com>
2025-04-14 21:40:27 +0200

    added proxy

--- netlify/functions/prayerTimesProxy.js
+++ netlify/functions/prayerTimesProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const proxy = createProxyMiddleware({
   target: 'https://mawaqit.net', // Target website
   changeOrigin: true,
   pathRewrite: {
-    '^/.netlify/functions/prayer-times': '/fr/w/amt-mosquee-as-sahaba-thonon-les-bains-74200-france?showOnly5PrayerTimes=0',
+    '^/.netlify/functions/prayer-times': '', // Remove the Netlify function prefix
   },
   onProxyRes: (proxyRes) => {
     // Remove X-Frame-Options header to allow embedding
@@ -12,18 +12,35 @@ const proxy = createProxyMiddleware({
   },
 });
 
-exports.handler = async (event, context) => {
+export const handler = (event) => {
   return new Promise((resolve, reject) => {
-    proxy(event, context, (err) => {
+    const req = {
+      method: event.httpMethod,
+      url: event.path.replace('/.netlify/functions/prayer-times', ''), // Ensure correct path
+      headers: event.headers,
+      body: event.body,
+    };
+
+    const res = {
+      setHeader: (name, value) => {
+        headers = headers || {};
+        headers[name] = value;
+      },
+      end: (body) => {
+        resolve({
+          statusCode: this.statusCode || 200,
+          headers: headers || {},
+          body,
+        });
+      },
+      statusCode: 200,
+    };
+
+    proxy(req, res, (err) => {
       if (err) {
         reject({
           statusCode: 500,
           body: 'Proxy error: ' + err.message,
-        });
-      } else {
-        resolve({
-          statusCode: 200,
-          body: 'Proxy working',
         });
       }
     });
