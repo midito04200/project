fd711d5b0fc4775487fdb74d8ff7ccd3786b006d
midito <miditochaoui@gmail.com>
2025-06-28 17:12:30 +0200

    replace PrayerTimes component with iframe for Mawaqit Prayer Times

--- src/components/PrayerTimesSection.jsx
+++ src/components/PrayerTimesSection.jsx
@@ -1,4 +1,3 @@
-import PrayerTimes from './PrayerTimes';
 import DonationSlider from './DonationSlider';
 
 export default function PrayerTimesSection() {
@@ -6,7 +5,15 @@ export default function PrayerTimesSection() {
     <div id="horaires" className="relative py-20 overflow-hidden">
       <DonationSlider opacity={0.85} />
       <div className="relative z-10">
-        <PrayerTimes />
+        <div className="w-full h-[600px] rounded-lg overflow-hidden">
+          <iframe
+            title="Mawaqit Prayer Times"
+            src="https://mawaqit.net/fr/w/amt-mosquee-as-sahaba-thonon-les-bains-74200-france?showOnly5PrayerTimes=0"
+            frameBorder="0"
+            scrolling="no"
+            className="w-full h-full"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
