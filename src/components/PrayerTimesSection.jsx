542fd05c316f6bb1afdedf0885ee1325efcde9a0
midito <miditochaoui@gmail.com>
2025-10-05 22:13:32 +0200

    mawaqit

--- src/components/PrayerTimesSection.jsx
+++ src/components/PrayerTimesSection.jsx
@@ -2,13 +2,23 @@ import DonationSlider from './DonationSlider';
 
 export default function PrayerTimesSection() {
   return (
-    <div id="horaires" className="relative py-20 overflow-hidden">
+    <section id="horaires" className="relative py-20">
       <DonationSlider opacity={0.85} />
-      <div className="relative z-10">
-        <div className="w-full h-[600px] rounded-lg overflow-hidden">
-          <iframe src="https://mawaqit.net/fr/w/thonon?showOnly5PrayerTimes=0" frameborder="0" scrolling="no" class="widget"></iframe>
+
+      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="bg-white rounded-lg overflow-hidden shadow-lg">
+          <div className="w-full h-[600px]">
+            <iframe
+              title="Mawaqit Prayer Times"
+              src="https://mawaqit.net/fr/w/amt-mosquee-as-sahaba-thonon-les-bains-74200-france?showOnly5PrayerTimes=0"
+              frameBorder="0"
+              scrolling="no"
+              loading="lazy"
+              className="w-full h-full"
+            ></iframe>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
